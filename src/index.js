8a60ccc3d6b62af70105ea60cb4fbec80cc537df
Cyfred Odarve <cyfredodarve@gmail.com>
2024-04-09 14:36:29 +0800

    2nd Commit of Code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import * as renderingMovieCards from './js/rendering-movie-cards';
+import * as searchByKeyword from './js/search-by-keyword';
+
+import * as openModal from './js/open-and-close-modal';
+import * as teamModal from './js/team-load-to-modal';
+import * as galleryMarkup from './js/create-gallery-markup';
+
+import * as modalFilm from './js/loading-into-modal';
+import * as openAndCloseModal from './js/open-and-close-modal';
+import './js/loader';
+
+import { refs } from './js/refs';
+import { onBtnPageClick } from './js/pagination';
+import { onBtnAddToLibrary } from './js/add-to-watched&queue';
+
+refs.pagination.addEventListener('click', onBtnPageClick);
+refs.btnWrapper.addEventListener('click', onBtnAddToLibrary);
